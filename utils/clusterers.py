c57a3877b6a2a9e98868d7074ea0b021e93d72be
Turfa Auliarachman <turfa_auliarachman@rocketmail.com>
2019-03-09 12:00:47 +0700

    Add best-first clusterer

---
+++ utils/clusterers.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+from functools import reduce
+from typing import List, Tuple, Dict, Iterable
+
+import numpy as np
+
+from utils.data_structures import UFDS
+
+
+def get_anaphora_scores_by_antecedent(m1_ids: Iterable[int], m2_ids: Iterable[int], scores: np.ndarray) -> \
+        Dict[int, List[Tuple[int, float]]]:
+    anaphora_scores_by_antecedent = {}
+
+    for m1_id, m2_id, score in zip(m1_ids, m2_ids, scores):
+        if m1_id not in anaphora_scores_by_antecedent:
+            anaphora_scores_by_antecedent[m1_id] = []
+
+        anaphora_scores_by_antecedent[m1_id].append((m2_id, score))
+
+    return anaphora_scores_by_antecedent
+
+
+class Clusterer(ABC):
+    def get_chains(self, anaphora_scores_by_antecedent: Dict[int, List[Tuple[int, float]]], threshold: float = 0.5) -> \
+            List[List[int]]:
+        ufds = UFDS()
+
+        for m1_id, m2_ids in anaphora_scores_by_antecedent.items():
+            ufds.init_id(m1_id)
+            passed_anaphora = list(filter(lambda x: x[1] > threshold, m2_ids))
+            choosed_pairs = self._choose_pairs(m1_id, passed_anaphora)
+
+            for choosed_pair in choosed_pairs:
+                ufds.join(m1_id, choosed_pair)
+
+        return ufds.get_chain_list()
+
+    @abstractmethod
+    def _choose_pairs(self, m1_id: int, anaphora_scores: List[Tuple[int, float]]) -> List[int]:
+        pass
+
+
+class BestFirstClusterer(Clusterer):
+    def _choose_pairs(self, m1_id: int, anaphora_scores: List[Tuple[int, float]]) -> List[int]:
+        if len(anaphora_scores) == 0:
+            return []
+
+        best_anaphora = reduce(lambda x, y: x if x[1] > y[1] else y, anaphora_scores, (-1, -1))
+        return [best_anaphora[0]]
